6d88c36a6e8e59ffcd0c1a8e4926bf9be6ce1721
{{brunoSandoval210}} <bruno.sandoval210@gmail.com>
2026-07-28 00:54:16 -0500

    first commit

---
+++ src/main/java/com/module/sblm/ModuleContractsApplication.java
@@ -0,0 +1,13 @@
+package com.module.sblm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleContractsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleContractsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/module/sblm/ModuleContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.sblm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleContractsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
